9d36993708b658ccc30cd5df102291bfcc6a551a
yusmi <yusmiaindayani@gmail.com>
2025-08-05 20:05:14 +0700

    feat: add "Pengujian" menu item to Sidebar with FlaskConical icon

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -13,6 +13,7 @@ import {
   User2Icon,
   HistoryIcon,
   MedalIcon,
+  FlaskConical,
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { logOut } from "@/actions/admin";
@@ -98,6 +99,11 @@ export function Sidebar({ className = "", username }: SidebarProps) {
       name: "Manajemen Nilai Siswa",
       icon: <MedalIcon className="w-5 h-5" />,
       href: "/scores",
+    },
+     {
+      name: "Pengujian",
+      icon: <FlaskConical className="w-5 h-5" />,
+      href: "/tests",
     },
     {
       name: "Riwayat Aktivitas",
